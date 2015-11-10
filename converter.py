e9e6b6452baad64e1e68bbb040762211bef6b8e5
Jim MacArthur <jim.macarthur@codethink.co.uk>
2015-11-10 18:00:19 +0000

    Split initializer-fix into separate function (no longer used); add implict reorderer

--- converter.py
+++ converter.py
@@ -19,6 +19,48 @@ def isComment(line):
 def isContinuation(line):
     return len(line)>5 and line[5] != ' ' and not isComment(line)
 
+def fixOldStyleInitializers(lines):
+    initRegex = re.compile('(?P<type>INTEGER|LOGICAL|CHARACTER)(?P<kind>\*\(?\d+\)?)?\s+(?P<ident>\S+)\s*\/(?P<value>\S+)\/(?P<tail>\s*,.*$)')
+    oldDeclarationRegex = re.compile('(?P<ident>\S+)\s*\/(?P<value>\S+)\/')
+    newLines = []
+    for lineno in range(0,len(lines)):
+        line = lines[lineno]
+        m = initRegex.search(line.rstrip())
+        if m:
+            print "Matched old-style initializer line"
+            newLine = "      %s%s :: %s = %s"%(m.group('type'), m.group('kind') or "", m.group('ident'), m.group('value'))
+
+            # Check for further declarations
+            declarations = m.group('tail').split(",")
+            newDeclarations = []
+            for d in declarations:
+                print "checking subsequent declaration %s"%d
+
+                if oldDeclarationRegex.search(d):
+                    newDeclarations.append("%s = %s"%(m.group('ident'), m.group('value')))
+                else:
+                    newDeclarations.append(d)
+            newLines.append(newLine + ",".join(newDeclarations) + "\n")
+        else:
+            newLines.append(line)
+    return newLines
+
+def fixImplicitStatements(lines):
+    implicitRegex = re.compile('^\s+IMPLICIT ', re.IGNORECASE)
+    programUnitStartRegex = re.compile('\s+(PROGRAM|SUBROUTINE|FUNCTION|BLOCK|INCLUDE)\s+', re.IGNORECASE)
+    insertPoint = 0
+    print "Reordering IMPLICIT statements..."
+    for lineno in range(0,len(lines)):
+        l = lines[lineno]
+        if implicitRegex.search(l.rstrip()):
+            lines.pop(lineno)
+            lines.insert(insertPoint, l)
+            print "Removed line from %d and inserted it at %d"%(lineno,insertPoint)
+        elif programUnitStartRegex.search(l.rstrip()):
+            insertPoint = lineno+1
+            print "Insert point is now %d"%insertPoint
+    return lines
+
 def fixFortran(filename):
     allLines = []
     print "Processing %s"%filename
@@ -41,21 +83,12 @@ def fixFortran(filename):
             print "Removed line %d and added it to the end of the preceding line"%(lineno)
     print "%d lines after rejoining"%len(allLines)
 
-    # Now do processing
-    initRegex = re.compile('(?P<type>INTEGER|LOGICAL|CHARACTER)(?P<kind>\*\(?\d+\)?)?\s+(?P<ident>\S+)\s*\/(?P<value>\S+)\/(?P<tail>\s*,.*$)')
-    lines = []
-    for lineno in range(0,len(allLines)):
-        line = allLines[lineno]
-        m = initRegex.search(line.rstrip())
-        if m:
-            print "Matched old-style initializer line"
-            newLine = "      %s%s :: %s = %s%s\n"%(m.group('type'), m.group('kind') or "", m.group('ident'), m.group('value'), m.group('tail'))
-            lines.append(newLine)
-        else:
-            lines.append(line)
+    # Process old-style initializers
+    #allLines = fixOldStyleInitializers(allLines)
+    allLines = fixImplicitStatements(allLines)
 
     f = open(os.path.join(filename), 'wt')
-    for l in lines:
+    for l in allLines:
         for sl in split_fortran_line_at_72(l):
             f.write(sl)
     f.close()
